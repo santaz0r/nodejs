d9846458616ec13d145ada5969ce4f58a08ede58
VictorM <neuron96@mail.ru>
2022-03-25 21:37:24 +0400

    extra hw 2

--- public/app.js
+++ public/app.js
@@ -1,19 +1,55 @@
 document.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  const title = event.target.dataset.title;
+  const submitBtn = document.querySelector(
+    `[data-type='submit'][data-id='${id}']`
+  );
+  const cancelBtn = document.querySelector(
+    `[data-type='cancel'][data-id='${id}']`
+  );
+  const editBtn = document.querySelector(`[data-type='edit'][data-id='${id}']`);
+  const remvoeBtn = document.querySelector(
+    `[data-type='remove'][data-id='${id}']`
+  );
+  const changeInput = document.querySelector(
+    `[data-type='inputField'][data-id='${id}']`
+  );
+  const span = document.querySelector(
+    `[data-type='inputSpan'][data-id='${id}']`
+  );
+
+  const changeArray = [
+    submitBtn,
+    cancelBtn,
+    editBtn,
+    remvoeBtn,
+    changeInput,
+    span,
+  ];
+  function setVisibility(array) {
+    array.forEach((item) => item.classList.toggle("visually-hidden"));
+  }
+
   if (event.target.dataset.type === "remove") {
-    const id = event.target.dataset.id;
     remove(id).then(() => {
       event.target.closest("li").remove();
     });
   }
   if (event.target.dataset.type === "edit") {
-    const id = event.target.dataset.id;
-    const title = event.target.dataset.title;
-    const newTitle = prompt("Введите новое название", title);
+    setVisibility(changeArray);
+  }
+  if (event.target.dataset.type === "submit") {
+    const newTitle = changeInput.value;
     if (newTitle !== null) {
       edit({ id, title: newTitle }).then(() => {
         event.target.closest("li").querySelector("span").innerText = newTitle;
       });
     }
+    setVisibility(changeArray);
+  }
+  if (event.target.dataset.type === "cancel") {
+    changeInput.value = title;
+    setVisibility(changeArray);
   }
 });
 
